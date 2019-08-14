8967542c38bd1995933ba07f184c995aa1b06950
Ehsan Ghafari <ehsanghafari88@gmail.com>
2019-08-13 19:54:09 -0700

    Add initial files

---
+++ js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var userFirstName = prompt('Welcome to my about me page. My name is Ehsan. What is your name?');
+
+var userLastName = prompt('Wohoo'+userFirstName+'I beleive every names are uniquie. What is your last name?');
+
+var userAge = prompt('That\'t great, How old are you?');
+
+
+
+// if(userName.toUpperCase() === 'YES'){
+//   alert('I like Game Of Thrones, Too');
+// } else {
+//   alert('What do you mean?!');
+// }
